61b34e43eadd2bdd96d9db3ab8b85b824361bb44
Tesmod <agboolafarouq11@gmail.com>
2025-03-31 05:07:00 +0100

    Arrays

--- 5. Arrays/index.js
+++ 5. Arrays/index.js
@@ -20,12 +20,29 @@ class MyArray {
         this.length--
         return lastItem
     }
+
+    shift(){
+        const firstElement = this.data[0]
+
+        for (let i = 0; i < i.length; i++) {
+            this.data[i] = this.data[i + 1]
+        }
+
+        
+        delete this.data[this.length - 1]
+        this.length--
+        return firstElement
+    }
 }
 
 const myNewArray = new MyArray()
 myNewArray.push('apple')
 myNewArray.push('mango')
 myNewArray.push('2')
-myNewArray.pop()
+myNewArray.push('2')
+// myNewArray.pop()
+myNewArray.shift()
 // console.log(myNewArray.get('0'))
+console.log(myNewArray);
+console.log(myNewArray.shift());
 console.log(myNewArray);
